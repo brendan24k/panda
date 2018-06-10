b5dc25a328f7c824aede515b562f0fad682394f2
BuildTools <unconfigured@null.spigotmc.org>
2018-06-10 10:01:34 +1000

    more variables

--- lunchmaker.js
+++ lunchmaker.js
@@ -1,2 +1,10 @@
 
 var greeting = 'Helo World!!';
+var isHot = false;
+var angry = 'Go Away!!';
+var house = {};
+house.windows = 4;
+house.color = 'blue';
+house.garden = 3;
+
+house['greeting'] = 'hi';
